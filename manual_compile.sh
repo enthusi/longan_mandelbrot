e4ec10508bb66090dc08aeba3f005da64ca7509c
enthusi <martin.wendt@gmx.de>
2020-06-28 14:00:58 +0200

    added files and snip1.*

---
+++ manual_compile.sh
@@ -0,0 +1,1 @@
+/home/enthusi/2020/riscv/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14/bin/riscv64-unknown-elf-gcc -O3 -fverbose-asm -march=rv32imac -mabi=ilp32 -S snip1.c
---
+++ snip1.h
@@ -0,0 +1,2 @@
+
+int add_asm();
